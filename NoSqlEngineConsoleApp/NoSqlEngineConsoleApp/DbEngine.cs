56b6c78b8f0e66fb96e532adfed779d05b0afa6f
Sebastian Dudzic <sebastian.dudzic@crimsonpine.com>
2017-09-14 20:42:27 +0200

    debug engine version with adding some data in tasks adn reading it

--- NoSqlEngineConsoleApp/NoSqlEngineConsoleApp/DbEngine.cs
+++ NoSqlEngineConsoleApp/NoSqlEngineConsoleApp/DbEngine.cs
@@ -10,16 +10,83 @@ namespace NoSqlEngineConsoleApp
 {
     class DbEngine
     {
-        protected static IMongoClient _client;
-        protected static IMongoDatabase _database;
+        private static IMongoClient _client;
+        private static IMongoDatabase _database;
+        private IMongoCollection<BsonDocument> tankMeasuresCollection;
 
         private const string DB_NAME = "db";
+        private const string TANK_MEASURES_NAME = "TankMeasures";
+
+        //debug
+        private System.Random random;
 
         public DbEngine()
         {
             _client = new MongoClient();
-            _client.DropDatabase(DB_NAME);
+            //_client.DropDatabase(DB_NAME);
             _database = _client.GetDatabase(DB_NAME);
+            tankMeasuresCollection = _database.GetCollection<BsonDocument>(TANK_MEASURES_NAME);
+
+            //debug
+            random = new System.Random();
+        }
+
+        public async Task RunAllTests()
+        {
+            await Task.WhenAll(RunTestAdd(), RunTestRead());
+        }
+
+        private async Task RunTestAdd()
+        {
+            while(true)
+            {
+                await Task.Delay(1000);
+                await AddTankMeasureAsync();           
+            }
+        }
+
+        private async Task RunTestRead()
+        {
+            while (true)
+            {
+                await Task.Delay(300);
+                await ReadCollectionCount(tankMeasuresCollection);
+            }
+        }
+
+        private async Task AddTankMeasureAsync()
+        {
+            Console.WriteLine("AddTankMeasureAsync");
+
+            var document = new BsonDocument
+            {
+                { "id", Guid.NewGuid().ToString("N") },
+                { "fuel", random.Next(0,1000) },
+            };
+
+            Console.WriteLine(document);
+
+            await tankMeasuresCollection.InsertOneAsync(document);
+        }
+
+        private async Task ReadCollectionCount(IMongoCollection<BsonDocument> collection)
+        {
+            var filter = new BsonDocument();
+            var count = 0;
+            using (var cursor = await collection.FindAsync(filter))
+            {
+                while (await cursor.MoveNextAsync())
+                {
+                    var batch = cursor.Current;
+                    foreach (var document in batch)
+                    {
+                        // process document
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Count: " + count);
         }
     }
 }
