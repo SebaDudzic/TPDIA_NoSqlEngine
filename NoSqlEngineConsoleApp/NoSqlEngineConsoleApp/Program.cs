b18524ab3d850f8695495e432f2e8718e149234b
Sebastian Dudzic <sebastian.dudzic@crimsonpine.com>
2017-09-14 19:54:22 +0200

    FIrst version

---
+++ NoSqlEngineConsoleApp/NoSqlEngineConsoleApp/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace TestMongo
+{
+    class Program
+    {
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+
+        static void Main(string[] args)
+        {
+            _client = new MongoClient();
+            _database = _client.GetDatabase("test");
+
+            List<Task> tasks = new List<Task>();
+
+            tasks.Add(Task.Run(InsertDataAsync));
+            tasks.Add(Task.Run(ReadData));
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.Read();
+        }
+
+        private static async Task ReadData()
+        {
+            var collection = _database.GetCollection<BsonDocument>("restaurants");
+            var filter = new BsonDocument();
+            var count = 0;
+            using (var cursor = await collection.FindAsync(filter))
+            {
+                while (await cursor.MoveNextAsync())
+                {
+                    var batch = cursor.Current;
+                    foreach (var document in batch)
+                    {
+                        // process document
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Count: " + count);
+        }
+
+        private static async Task InsertDataAsync()
+        {
+            var document = new BsonDocument
+{
+    { "address" , new BsonDocument
+        {
+            { "street", "2 Avenue" },
+            { "zipcode", "10075" },
+            { "building", "1480" },
+            { "coord", new BsonArray { 73.9557413, 40.7720266 } }
+        }
+    },
+    { "borough", "Manhattan" },
+    { "cuisine", "Italian" },
+    { "grades", new BsonArray
+        {
+            new BsonDocument
+            {
+                { "date", new DateTime(2014, 10, 1, 0, 0, 0, DateTimeKind.Utc) },
+                { "grade", "A" },
+                { "score", 11 }
+            },
+            new BsonDocument
+            {
+                { "date", new DateTime(2014, 1, 6, 0, 0, 0, DateTimeKind.Utc) },
+                { "grade", "B" },
+                { "score", 17 }
+            }
+        }
+    },
+    { "name", "Vella" },
+    { "restaurant_id", "41704620" }
+};
+
+            var collection = _database.GetCollection<BsonDocument>("restaurants");
+            await collection.InsertOneAsync(document);
+        }
+    }
+}
