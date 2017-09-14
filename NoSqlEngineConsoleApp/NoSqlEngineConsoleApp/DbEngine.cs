8003e4f7a4c906e9563af52e25515ace7b70b2b1
Sebastian Dudzic <sebastian.dudzic@crimsonpine.com>
2017-09-14 20:09:58 +0200

    missing settings

--- NoSqlEngineConsoleApp/NoSqlEngineConsoleApp/DbEngine.cs
+++ NoSqlEngineConsoleApp/NoSqlEngineConsoleApp/DbEngine.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace NoSqlEngineConsoleApp
 {
     class DbEngine
     {
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+
+        private const string DB_NAME = "db";
+
+        public DbEngine()
+        {
+            _client = new MongoClient();
+            _client.DropDatabase(DB_NAME);
+            _database = _client.GetDatabase(DB_NAME);
+        }
     }
 }
