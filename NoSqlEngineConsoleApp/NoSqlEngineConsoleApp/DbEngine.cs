feb432f73121cd2763e2eada69e9f9b02d40eb47
Sebastian Dudzic <sebastian.dudzic@crimsonpine.com>
2017-09-14 21:19:58 +0200

    Adding tank measure and nozzle measure interfaces

--- NoSqlEngineConsoleApp/NoSqlEngineConsoleApp/DbEngine.cs
+++ NoSqlEngineConsoleApp/NoSqlEngineConsoleApp/DbEngine.cs
@@ -13,9 +13,13 @@ namespace NoSqlEngineConsoleApp
         private static IMongoClient _client;
         private static IMongoDatabase _database;
         private IMongoCollection<BsonDocument> tankMeasuresCollection;
+        private IMongoCollection<BsonDocument> nozzleMeasuresCollection;
+        private IMongoCollection<BsonDocument> refuelsCollection;
 
         private const string DB_NAME = "db";
         private const string TANK_MEASURES_NAME = "TankMeasures";
+        private const string NOZZLE_MEASURES_NAME = "NozzleMeasures";
+        private const string REFUELS_NAME = "Refuels";
 
         //debug
         private System.Random random;
@@ -23,52 +27,81 @@ namespace NoSqlEngineConsoleApp
         public DbEngine()
         {
             _client = new MongoClient();
-            //_client.DropDatabase(DB_NAME);
+            _client.DropDatabase(DB_NAME);
             _database = _client.GetDatabase(DB_NAME);
             tankMeasuresCollection = _database.GetCollection<BsonDocument>(TANK_MEASURES_NAME);
-
+            nozzleMeasuresCollection = _database.GetCollection<BsonDocument>(NOZZLE_MEASURES_NAME);
+            refuelsCollection = _database.GetCollection<BsonDocument>(REFUELS_NAME);
             //debug
             random = new System.Random();
         }
 
-        public async Task RunAllTests()
-        {
-            await Task.WhenAll(RunTestAdd(), RunTestRead());
-        }
+        //--Public_Interface------------------------------------//
 
-        private async Task RunTestAdd()
+        public async void AddTankMeasure(TankMeasure data)
         {
-            while(true)
-            {
-                await Task.Delay(1000);
-                AddTankMeasure();           
-            }
-        }
-
-        private async Task RunTestRead()
-        {
-            while (true)
-            {
-                await Task.Delay(300);
-                ReadCollectionCount(tankMeasuresCollection);
-            }
-        }
-
-
-        private async void AddTankMeasure()
-        {
-            Console.WriteLine("AddTankMeasureAsync");
+            Console.WriteLine("AddTankMeasure");
 
             var document = new BsonDocument
             {
                 { "id", Guid.NewGuid().ToString("N") },
-                { "fuel", random.Next(0,1000) },
+                { "date", data.date },
+                { "locationID", data.locationID },
+                { "meterID", data.meterID },
+                { "tankID", data.tankID },
+                { "fuelHeight", data.fuelHeight },
+                { "fuelCapacity", data.fuelCapacity },
+                { "fuelTemperature", data.fuelTemperature },
+                { "waterHeight", data.waterHeight },
+                { "waterCapacity", data.waterCapacity },
             };
 
             Console.WriteLine(document);
 
             await tankMeasuresCollection.InsertOneAsync(document);
         }
+
+        public async void AddNozzleMeasure(NozzleMeasure data)
+        {
+            Console.WriteLine("AddNozzleMeasure");
+
+            var document = new BsonDocument
+            {
+                { "id", Guid.NewGuid().ToString("N") },
+                { "date", data.date },
+                { "locationID", data.locationID },
+                { "fuelGun", data.fuelGun },
+                { "tankID", data.tankID },
+                { "literCounter", data.literCounter },
+                { "totalCounter", data.totalCounter },
+                { "status", data.status },
+            };
+
+            Console.WriteLine(document);
+
+            await tankMeasuresCollection.InsertOneAsync(document);
+        }
+
+        //private async void AddNozzleMeasure(NozzleMeasure data)
+        //{
+        //    Console.WriteLine("AddNozzleMeasure");
+
+        //    var document = new BsonDocument
+        //    {
+        //        { "id", Guid.NewGuid().ToString("N") },
+        //        { "date", data.date },
+        //        { "locationID", data.locationID },
+        //        { "fuelGun", data.fuelGun },
+        //        { "tankID", data.tankID },
+        //        { "literCounter", data.literCounter },
+        //        { "totalCounter", data.totalCounter },
+        //        { "status", data.status },
+        //    };
+
+        //    Console.WriteLine(document);
+
+        //    await tankMeasuresCollection.InsertOneAsync(document);
+        //}
 
         private async void ReadCollectionCount(IMongoCollection<BsonDocument> collection)
         {
@@ -89,5 +122,57 @@ namespace NoSqlEngineConsoleApp
 
             Console.WriteLine("Count: " + count);
         }
+
+        //--Public_Interface_END------------------------------------//
+
+        //--DEBUG----------------//
+
+        public async Task RunAllTests()
+        {
+            await Task.WhenAll(RunTestAddTankMeasure(), RunTestAddNozzleMeasure(), RunTestRead());
+        }
+
+        private async Task RunTestAddTankMeasure()
+        {
+            while (true)
+            {
+                await Task.Delay(1000);
+                AddTankMeasure(new TankMeasure(System.DateTime.Now,
+                    random.Next(0, 5),
+                    random.Next(0, 5),
+                    random.Next(0, 5),
+                    (float)random.NextDouble(),
+                    (float)random.NextDouble(),
+                    (float)random.NextDouble(),
+                    (float)random.NextDouble(),
+                    (float)random.NextDouble());
+            }
+        }
+
+        private async Task RunTestAddNozzleMeasure()
+        {
+            while (true)
+            {
+                await Task.Delay(750);
+                AddNozzleMeasure(new NozzleMeasure(System.DateTime.Now,
+                    random.Next(0, 5),
+                    random.Next(0, 5),
+                    random.Next(0, 5),
+                    (float)random.NextDouble(),
+                    (float)random.NextDouble(),
+                    random.Next(0, 5)));
+            }
+        }
+
+        private async Task RunTestRead()
+        {
+            while (true)
+            {
+                await Task.Delay(300);
+                ReadCollectionCount(tankMeasuresCollection);
+            }
+        }
+
+        //--DEBUG_END----------------//
     }
 }
